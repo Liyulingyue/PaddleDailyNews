833fe6e011ab34b3c73d3e428ad8081b9107580c
gouzi <530971494@qq.com>
2023-11-10 17:38:15 +0800

    add get org repos; Modify org; add ci

--- github_help.py
+++ github_help.py
@@ -6,6 +6,7 @@ from github.PullRequest import PullRequest
 from loguru import logger
 
 if TYPE_CHECKING:
+    from github.Organization import Organization
     from github.PaginatedList import PaginatedList
     from github.Repository import Repository
 
@@ -14,9 +15,9 @@ if TYPE_CHECKING:
 class GithubHelper:
     def __init__(self, token: str, org: str = "paddlepaddle") -> None:
         self.g = Github(auth=Auth.Token(token), per_page=100)
-        self.data_ccashe: dict[str, list[PullRequest]] = {}
+        self.data_ccashe: dict[str, list[PullRequest] | None] = {}
         # 请不要修改 org, 如果需要修改请重新创建一个 GithubHelper
-        self.__org:str = org
+        self.__org:Organization = self.g.get_organization(org)
 
     # for debug
     def get_user_name(self) -> str:
@@ -25,7 +26,11 @@ class GithubHelper:
         """
         return self.g.get_user().login
 
-    def get_ccashe(self) -> dict[str, list[PullRequest]]:
+    # for debug
+    def get_org_name(self) -> str:
+        return self.__org.login
+
+    def get_ccashe(self) -> dict[str, list[PullRequest] | None]:
         """
         获取已缓存的数据
         """
@@ -53,14 +58,13 @@ class GithubHelper:
                 case _:
                     ...
             return t
-
-        logger.info(f"requast repo: {self.__org}/{repo_name} branch: {base} start")
+        logger.info(f"requast repo: {self.__org.login}/{repo_name} branch: {base} start")
         # 转换时区
         start_time = convert_time_zone(start_time)
         end_time = convert_time_zone(end_time)
         logger.info(f"request start time: {start_time}, end time: {end_time}")
 
-        repo: Repository = self.g.get_repo(f"{self.__org}/{repo_name}")
+        repo: Repository = self.g.get_repo(f"{self.__org.login}/{repo_name}")
         res_list: list[PullRequest] = []
         pulls: PaginatedList[PullRequest] = repo.get_pulls(state="all", base=base)
         for i in pulls:
@@ -75,8 +79,25 @@ class GithubHelper:
         if write_cache:
             self.data_ccashe[repo_name] = res_list
 
-        logger.info(f"request repo: {self.__org}/{repo_name} branch: {base} end")
+        logger.info(f"request repo: {self.__org.login}/{repo_name} branch: {base} end")
         logger.debug(f"request len: {len(res_list)}")
         return res_list
+
+    def GetRepoList(self, write_cache:bool = True) -> list[str]:
+        """
+        获取当前组织下所有的仓库
+        """
+        logger.info(f"start get org repos: {self.__org.login}")
+        # 为什么不用all, 防止一些研发大哥把 private 的项目公开出来
+        repos: PaginatedList[Repository] = self.__org.get_repos(type="public")
+        res: list[str] = []
+        for i in repos:
+            res.append(i.name)
+            if write_cache and i.name not in self.data_ccashe.keys():
+                logger.debug(f"write cache {self.__org.login}: {i.name}")
+                self.data_ccashe[i.name] = None
+
+        logger.info(f"end get org repos: {self.__org.login}")
+        return res
 
 
