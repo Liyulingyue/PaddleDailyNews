16f18cabd786e7f33bffb340ba3e6427f48be19e
Liyulingyue <852433440@qq.com>
2023-11-29 23:55:28 +0800

    add something

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from configure import *
 from loguru import logger
 
 from github_helper import CacheMode, GithubHelper
-
+from statistic_helper import StatisticHelper
+from layout_helper import LayoutHelper
 
 def log_init():
     # TODO(gouzil): 改个前缀
@@ -22,3 +23,8 @@ g = GithubHelper(GITHUB_TOKEN)
 print(g.get_user_name())
 g.RefreshData([CacheMode.PR, CacheMode.ISSUES], datetime(2023, 11, 18), ["paddle"])
 g.GetRepoList()
+
+s_helper = StatisticHelper()
+l_helper = LayoutHelper()
+l_helper.generate_layout(s_helper)
+l_helper.Export2MarkdownFile()
