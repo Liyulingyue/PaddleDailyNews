e8f5db03de9cdf08282ff9f589212d50de0cd132
Liyulingyue <852433440@qq.com>
2023-12-03 21:30:12 +0800

    add something

--- layout_helper.py
+++ layout_helper.py
@@ -1,5 +1,5 @@
 from statistic_helper import *  # noqa: F403
-
+from llm_chat import *
 
 class LayoutHelper(object):
     def __init__(self):
@@ -7,36 +7,13 @@ class LayoutHelper(object):
         self.MarkDown_str = ""
 
     def generate_layout(self, s_helper: StatisticHelper):  # noqa: F405
-        self.MarkDown_str = f"""
-# 飞桨日报
-{s_helper.year}年{s_helper.month}月{s_helper.day}日 PR:{s_helper.pr_num}条 ISSUE:{s_helper.issue_num}条
+        self.MarkDown_str = ""
+        self.MarkDown_str += self.generate_layout_header(s_helper)
+        self.MarkDown_str += self.generate_layout_summary(s_helper)
+        self.MarkDown_str += self.generate_layout_PR_infor(s_helper)
+        self.MarkDown_str += self.generate_layout_Issue_infor(s_helper)
 
----
-
-## 概述
-*简述：概述当天PaddlePaddle社区的活跃情况，提及PR和ISSUE的总数，并突出一些重要或热点的内容。*
-
----
-
-## PR聚焦
-*数据分析：对当日PR的统计数据进行分析，如提交者的地域分布、PR涉及的模块分布等。*
-
-1. **[PR#XXXX](https://github.com/PaddlePaddle/Paddle/pull/XXXX)**
-    - 内容介绍：*详细描述：对此次PR的核心内容进行简要描述，如功能增加、性能提升或BUG修复等。*
-2. **[PR#XXXX](https://github.com/PaddlePaddle/Paddle/pull/XXXX)**
-    - 内容介绍: *详细描述：针对此PR的创新点、重要性或影响范围进行描述。*
-
----
-
-## ISSUE追踪
-*数据分析：对当日ISSUE的统计，如问题类型分布、解决问题所消耗的时间等。*
-
-1. **[ISSUE#XXXX](https://github.com/PaddlePaddle/Paddle/issues/XXXX)**
-    - 内容介绍: *详细描述：描述此ISSUE的主要问题、影响范围及当前状态。*
-2. **[ISSUE#XXXX](https://github.com/PaddlePaddle/Paddle/issues/XXXX)**
-    - 内容介绍: *详细描述：对另一热点ISSUE进行简要介绍，引发读者关注。*
-
----
+        self.MarkDown_str += f"""
 
 ## 贡献者荣誉榜
 
@@ -50,11 +27,6 @@ class LayoutHelper(object):
 2. *贡献者名称：简要描述其贡献内容。*
 3. *贡献者名称：简要描述其贡献内容。*
 
-### 讨论
-1. *贡献者名称：对此贡献者的贡献内容进行简要描述，并链接到其个人主页。*
-2. *贡献者名称：简要描述其贡献内容。*
-3. *贡献者名称：简要描述其贡献内容。*
-
 ---
 
 感谢各位开发者对PaddlePaddle的持续关注和贡献，让我们一起推动深度学习技术的发展和应用。期待明天更多的精彩！
@@ -63,3 +35,61 @@ class LayoutHelper(object):
     def Export2MarkdownFile(self, path="DailyReport.md"):
         with open(path, "w", encoding="utf8") as f:
             f.write(self.MarkDown_str)
+
+    def generate_layout_header(self, s_helper: StatisticHelper):
+        Markdown_str = f"""
+# 飞桨日报
+{s_helper.year}年{s_helper.month}月{s_helper.day}日 PR:{s_helper.pr_num}条 ISSUE:{s_helper.issue_num}条
+
+---
+
+        """
+        return Markdown_str
+
+    def generate_layout_summary(self, s_helper: StatisticHelper):
+        Markdown_str = f"""
+## 概述
+*简述：概述当天PaddlePaddle社区的活跃情况，提及PR和ISSUE的总数，并突出一些重要或热点的内容。*
+
+---
+
+        """
+        return Markdown_str
+
+    def generate_layout_PR_infor(self, s_helper: StatisticHelper):
+        score_threshold = 0 if len(s_helper.pr_list)<3 else max(0, s_helper.pr_list[2]["score"])
+        Markdown_str = ""
+        Markdown_str += f"""
+## PR聚焦
+*数据分析：对当日PR的统计数据进行分析，如提交者的地域分布、PR涉及的模块分布等。*
+        
+        """
+        for pr_infor in s_helper.pr_list:
+            if pr_infor["score"]>=score_threshold:
+                Markdown_str += f"""
+1. **[#{pr_infor['id']}](https://github.com/PaddlePaddle/Paddle/pull/{pr_infor['id']})**
+    - 内容介绍：{get_summary_of_a_change(pr_infor["introduction"])}
+    - 量化评分：{pr_infor["score"]}
+                """
+
+        Markdown_str += f"""
+
+---
+
+        """
+        return Markdown_str
+
+    def generate_layout_Issue_infor(self, s_helper: StatisticHelper):
+        Markdown_str = f"""
+## ISSUE追踪
+*数据分析：对当日ISSUE的统计，如问题类型分布、解决问题所消耗的时间等。*
+
+1. **[ISSUE#XXXX](https://github.com/PaddlePaddle/Paddle/issues/XXXX)**
+    - 内容介绍: *详细描述：描述此ISSUE的主要问题、影响范围及当前状态。*
+2. **[ISSUE#XXXX](https://github.com/PaddlePaddle/Paddle/issues/XXXX)**
+    - 内容介绍: *详细描述：对另一热点ISSUE进行简要介绍，引发读者关注。*
+
+---
+
+        """
+        return Markdown_str
--- statistic_helper.py
+++ statistic_helper.py
@@ -1,4 +1,7 @@
 from github_helper import GithubHelper, CacheMode
+import requests
+from llm_chat import *
+import traceback
 
 class StatisticHelper(object):
     # 对githubhelper中的信息进行统计
@@ -9,6 +12,7 @@ class StatisticHelper(object):
         self.day = date[2]
         self.pr_num = 0
         self.issue_num = 0
+        self.pr_list = []
 
     def refresh_number(self, g_helper: GithubHelper):
         g_helper.get_ccashe()
@@ -16,3 +20,25 @@ class StatisticHelper(object):
         cache_issue = g_helper.get_ccashe("paddle", CacheMode.ISSUES)
         self.pr_num = len(cache_pr)
         self.issue_num = len(cache_issue)
+
+    def get_score_of_pr(self, g_helper: GithubHelper):
+        g_helper.get_ccashe()
+        cache_pr = g_helper.get_ccashe("paddle")
+        self.pr_list = []
+        for pr in cache_pr:
+            print(f"make score in {pr.number}, total pr number is {len(cache_pr)}")
+            title = pr.title
+            id = pr.number
+            user = pr.user
+            diff_url = pr.diff_url
+            try:
+                r = requests.get(diff_url)
+                score, comments, introduction = get_score_of_a_change(r.text)
+            except:
+                score = -1
+                comments = ""
+                introduction = ""
+                print(traceback.format_exc())
+            self.pr_list.append({"title": title, "id": id, "user": user, "score": score, "comments": comments, "introduction":introduction})
+
+        self.pr_list.sort(key=lambda x: x['score'], reverse=True)
