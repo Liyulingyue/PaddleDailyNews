8bcf80866ff8bf83feae40cd1cf16d57cb76a4d2
Liyulingyue <852433440@qq.com>
2023-12-13 23:54:52 +0800

    add something

--- layout_helper.py
+++ layout_helper.py
@@ -9,28 +9,12 @@ class LayoutHelper(object):
     def generate_layout(self, s_helper: StatisticHelper):  # noqa: F405
         self.MarkDown_str = ""
         self.MarkDown_str += self.generate_layout_header(s_helper)
-        self.MarkDown_str += self.generate_layout_summary(s_helper)
+        # self.MarkDown_str += self.generate_layout_summary(s_helper)
         self.MarkDown_str += self.generate_layout_PR_infor(s_helper)
         self.MarkDown_str += self.generate_layout_Issue_infor(s_helper)
-
-        self.MarkDown_str += f"""
-
-## 贡献者荣誉榜
-
-### PR
-1. *贡献者名称：对此贡献者的贡献内容进行简要描述，并链接到其个人主页。*
-2. *贡献者名称：简要描述其贡献内容。*
-3. *贡献者名称：简要描述其贡献内容。*
-
-### Reviewer
-1. *贡献者名称：对此贡献者的贡献内容进行简要描述，并链接到其个人主页。*
-2. *贡献者名称：简要描述其贡献内容。*
-3. *贡献者名称：简要描述其贡献内容。*
-
----
-
-感谢各位开发者对PaddlePaddle的持续关注和贡献，让我们一起推动深度学习技术的发展和应用。期待明天更多的精彩！
-        """
+        self.MarkDown_str += self.generate_layout_pr_ranks(s_helper)
+        self.MarkDown_str += self.generate_layout_issue_ranks(s_helper)
+        self.MarkDown_str += self.generate_layout_ender(s_helper)
 
     def Export2MarkdownFile(self, path="DailyReport.md"):
         with open(path, "w", encoding="utf8") as f:
@@ -59,11 +43,17 @@ class LayoutHelper(object):
     def generate_layout_PR_infor(self, s_helper: StatisticHelper):
         score_threshold = 0 if len(s_helper.pr_list)<3 else max(0, s_helper.pr_list[2]["score"])
         Markdown_str = ""
-        Markdown_str += f"""
+        if 0:
+            Markdown_str += f"""
 ## PR聚焦
 *数据分析：对当日PR的统计数据进行分析，如提交者的地域分布、PR涉及的模块分布等。*
         
-        """
+            """
+        else:
+            Markdown_str += f"""
+## PR聚焦
+
+            """
         for pr_infor in s_helper.pr_list:
             if pr_infor["score"]>=score_threshold:
                 Markdown_str += f"""
@@ -82,9 +72,15 @@ class LayoutHelper(object):
     def generate_layout_Issue_infor(self, s_helper: StatisticHelper):
         score_threshold = 0 if len(s_helper.issue_list)<3 else max(0, s_helper.issue_list[2]["score"])
         Markdown_str = ""
-        Markdown_str += f"""
+        if 0:
+            Markdown_str += f"""
+    ## ISSUE追踪
+    *数据分析：对当日ISSUE的统计，如问题类型分布、解决问题所消耗的时间等。*
+    
+            """
+        else:
+            Markdown_str += f"""
 ## ISSUE追踪
-*数据分析：对当日ISSUE的统计，如问题类型分布、解决问题所消耗的时间等。*
 
         """
         for issue_infor in s_helper.issue_list:
@@ -102,3 +98,39 @@ class LayoutHelper(object):
                 """
         return Markdown_str
 
+    def generate_layout_pr_ranks(self, s_helper: StatisticHelper):
+        Markdown_str = ""
+        Markdown_str += f"""
+## 贡献者荣誉榜
+
+### PR
+        """
+        for infor in s_helper.pr_rank_list:
+            Markdown_str += f"""
+1. {infor["user"]}：提交PR{infor["times"]}次
+            """
+        return Markdown_str
+
+    def generate_layout_issue_ranks(self, s_helper: StatisticHelper):
+        Markdown_str = ""
+        Markdown_str += f"""
+
+### ISSUE
+        """
+        for infor in s_helper.issue_rank_list:
+            Markdown_str += f"""
+1. {infor["user"]}：提交ISSUE{infor["times"]}次
+            """
+        Markdown_str += f"""
+
+---
+
+        """
+        return Markdown_str
+
+    def generate_layout_ender(self, s_helper: StatisticHelper):
+        MarkDown_str = f"""
+
+感谢各位开发者对PaddlePaddle的持续关注和贡献，让我们一起推动深度学习技术的发展和应用。期待明天更多的精彩！
+        """
+        return MarkDown_str
